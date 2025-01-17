3653e293ba9b4baf490cc43acd68c20fb1317ae6
MRezaKarimi <mr.karimi77@yahoo.com>
2025-01-17 10:05:38 +0330

    feat: get year  and month from provider

--- src/components/calendar/CalendarTitle.tsx
+++ src/components/calendar/CalendarTitle.tsx
@@ -1,8 +1,14 @@
+import { useContext } from "react";
+import { DateContext } from "../../contexts/DateContext";
+import { getMonthName } from "../../lib/date-utils/month-name";
+
 export default function CalendarTitle() {
   return (
     <div className="text-base">
-      <span className="text-sky-800 font-medium pe-1">اردیبهشت</span>
-      <span className="text-gray-500 font-medium">1403</span>
+      <span className="text-sky-800 font-medium pe-1">
+        {getMonthName(useContext(DateContext).month)}
+      </span>
+      <span className="text-gray-500 font-medium">{useContext(DateContext).year}</span>
     </div>
   );
 }
