2f611cfd0ca183eb105c1b29bad32545b4d45995
MRezaKarimi <mr.karimi77@yahoo.com>
2025-01-17 14:01:14 +0330

    feat: add isEqual method

--- src/lib/date-utils/custom-date.ts
+++ src/lib/date-utils/custom-date.ts
@@ -1,3 +1,5 @@
+import moment from "moment-jalaali";
+
 export class CustomDate {
   constructor(
     public year: number,
@@ -8,5 +10,13 @@ export class CustomDate {
 
   toString() {
     return `${this.year} ${this.month} ${this.day}`;
+  }
+
+  isEqual(date: moment.Moment) {
+    return (
+      date.jYear() === this.year &&
+      date.jMonth() + 1 === this.month &&
+      date.jDate() === this.day
+    );
   }
 }
