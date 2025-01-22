03f46d38abe4c97fe909e00ae2c3a6237788bc1b
MRezaKarimi <mr.karimi77@yahoo.com>
2025-01-22 21:48:44 +0330

    feat: add active and isGray props to CalendarCell

--- src/components/calendar/CalendarCell.tsx
+++ src/components/calendar/CalendarCell.tsx
@@ -1,13 +1,15 @@
 interface CalendarCellProps {
   day: number | string;
+  isGray: boolean;
+  active: boolean;
 }
 
-export default function CalendarCell({ day }: CalendarCellProps) {
+export default function CalendarCell({ day, active, isGray }: CalendarCellProps) {
   return (
     <div
-      className={`p-2 text-center rounded-xl font-medium text-base text-gray-600 ${
-        day === 22 && "!text-sky-900 bg-sky-100 ring-1"
-      }`}
+      className={`size-9 p-2 text-center rounded-full font-medium text-base text-gray-600 ${
+        active && "!text-sky-800 bg-sky-100 ring-1"
+      } ${isGray && "!text-gray-300"}`}
     >
       {day}
     </div>
