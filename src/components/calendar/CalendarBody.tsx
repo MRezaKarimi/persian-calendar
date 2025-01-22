a488d46ff8e786f0a25766ee13ca896f9007a35d
MRezaKarimi <mr.karimi77@yahoo.com>
2025-01-22 21:51:28 +0330

    feat: integrate date context and enhance CalendarBody with dynamic day rendering

--- src/components/calendar/CalendarBody.tsx
+++ src/components/calendar/CalendarBody.tsx
@@ -1,15 +1,26 @@
+import { useContext, useMemo } from "react";
 import CalendarCell from "./CalendarCell";
 import CalendarWeekDays from "./CalendarWeekDays";
+import { DateContext } from "../../contexts/DateContext";
+import { getDateRange } from "../../lib/date-utils/date-range";
+import moment from "moment-jalaali";
+
+const today = moment();
 
 export default function CalendarBody() {
-  const days = Array.from({ length: 42 }, (_, i) => i + 1);
+  const { month, year } = useContext(DateContext);
+  const days = useMemo(() => getDateRange(year, month), [year, month]);
 
   return (
     <div className="grid grid-cols-7 gap-1">
       <CalendarWeekDays />
 
-      {days.map((day) => (
-        <CalendarCell day={day} />
+      {days.map((dayObj) => (
+        <CalendarCell
+          day={dayObj.day}
+          isGray={dayObj.month !== month}
+          active={dayObj.isEqual(today)}
+        />
       ))}
     </div>
   );
