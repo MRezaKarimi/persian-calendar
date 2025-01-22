5c475bd7555cd6c8f4c32c3e623343f87e877de8
MRezaKarimi <mr.karimi77@yahoo.com>
2025-01-22 21:49:22 +0330

    feat: integrate date context for month navigation in CalendarNavigation

--- src/components/calendar/CalendarNavigation.tsx
+++ src/components/calendar/CalendarNavigation.tsx
@@ -1,12 +1,21 @@
 import { ArrowLeft01Icon, ArrowRight01Icon } from "hugeicons-react";
-import { ReactNode } from "react";
+import { DateContext } from "../../contexts/DateContext";
+import { useContext, ReactNode } from "react";
 
 export default function CalendarNavigation({ children }: { children: ReactNode }) {
+  const dateContext = useContext(DateContext);
+
   return (
-    <div className="flex justify-between items-center gap-3 w-full mb-2 px-2">
-      <ArrowRight01Icon className="cursor-pointer" />
+    <div className="flex justify-between items-center gap-3 w-full mb-2 px-2 select-none">
+      <ArrowRight01Icon
+        className="cursor-pointer"
+        onClick={() => dateContext.previousMonth()}
+      />
       {children}
-      <ArrowLeft01Icon className="cursor-pointer" />
+      <ArrowLeft01Icon
+        className="cursor-pointer"
+        onClick={() => dateContext.nextMonth()}
+      />
     </div>
   );
 }
