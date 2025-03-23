37491c25ed9c3b54c671cf9ea220328a8a18f7bb
MRezaKarimi <mr.karimi77@yahoo.com>
2025-03-23 16:40:21 +0330

    fix: improve ux by increasing end padding

--- src/components/calendar/CalendarTitle.tsx
+++ src/components/calendar/CalendarTitle.tsx
@@ -5,7 +5,7 @@ import { getMonthName } from "../../lib/date-utils/month-name";
 export default function CalendarTitle() {
   return (
     <div className="text-base">
-      <span className="text-sky-800 font-medium pe-1">
+      <span className="text-sky-800 font-medium pe-2">
         {getMonthName(useContext(DateContext).month)}
       </span>
       <span className="text-gray-500 font-medium">{useContext(DateContext).year}</span>
