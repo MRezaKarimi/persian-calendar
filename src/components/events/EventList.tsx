e6b8c1c3292731e0d97d9313f942561062794e73
MRezaKarimi <mr.karimi77@yahoo.com>
2025-03-25 19:26:06 +0330

    feat: update EventList to use EventsContext and render events dynamically

--- src/components/events/EventList.tsx
+++ src/components/events/EventList.tsx
@@ -1,12 +1,14 @@
+import { useContext } from "react";
+import { EventsContext } from "../../contexts/EventsContext";
 import EventItem from "./EventItem";
 
 export default function EventList() {
+  const { events } = useContext(EventsContext);
   return (
-    <div className="flex flex-col p-4 border-s">
-      <EventItem />
-      <EventItem />
-      <EventItem />
-      <EventItem />
+    <div className="flex flex-col p-3 border-s overflow-auto">
+      {events.map((event) => (
+        <EventItem key={event.id} event={event} />
+      ))}
     </div>
   );
 }
