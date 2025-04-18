88e66902b754c80c23f5ab6c044179463e558a6c
MRezaKarimi <mr.karimi77@yahoo.com>
2025-04-18 18:36:07 +0330

    fix: update CalendarCell styles for active and holiday states

--- src/components/calendar/CalendarCell.tsx
+++ src/components/calendar/CalendarCell.tsx
@@ -18,10 +18,10 @@ export default function CalendarCell({
       className={classNames(
         "size-9 p-2 text-center rounded-full font-medium text-base text-gray-600",
         {
-          "text-sky-800 bg-sky-100 ring-1": active,
+          "text-sky-800 bg-sky-100 ring-1": active && !holiday,
           "opacity-40": isGray,
           "text-red-600": holiday,
-          "!text-red-600 bg-red-50 ring-1 ring-red-600": holiday && active,
+          "text-red-600 bg-red-50 ring-1 ring-red-600": holiday && active,
         }
       )}
     >
