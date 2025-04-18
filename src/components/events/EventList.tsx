c452741b37b68efd3161f6b2bc0abc13b7f72be3
MRezaKarimi <mr.karimi77@yahoo.com>
2025-04-18 18:38:23 +0330

    feat: display message when no events are available for the selected date

--- src/components/events/EventList.tsx
+++ src/components/events/EventList.tsx
@@ -1,9 +1,18 @@
 import { useContext } from "react";
 import { EventsContext } from "../../contexts/EventsContext";
 import EventItem from "./EventItem";
+import { DateContext } from "../../contexts/DateContext";
 
 export default function EventList() {
   const { events } = useContext(EventsContext);
+  const { year } = useContext(DateContext);
+
+  if (events.length === 0) {
+    return (
+      <div className="text-gray-500 text-sm text-center py-6">{`رویداد های سال ${year} وجود ندارد`}</div>
+    );
+  }
+
   return (
     <div className="flex flex-col p-3 border-s overflow-auto">
       {events.map((event) => (
